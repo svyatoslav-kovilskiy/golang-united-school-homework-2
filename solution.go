75ad37c546b436b61c369ff1b3b325f23f4d1410
Svyatoslav <svyatoslav_kovylskiy@epam.com>
2022-02-21 16:04:08 +0500

    Added finctions for calculate square in solution.go

--- solution.go
+++ solution.go
@@ -1,5 +1,8 @@
 package square
 
+import (
+	"math"
+)
 // Define custom int type to hold sides number and update CalcSquare signature by replacing #yourTypeNameHere#
 
 // Define constants to represent 0, 3 and 4 sides.  Test uses mnemos: SidesTriangle(==3), SidesSquare(==4), SidesCircle(==0)
@@ -7,6 +10,42 @@ package square
 // CalcSquare(10.0, SidesTriangle)
 // CalcSquare(10.0, SidesSquare)
 // CalcSquare(10.0, SidesCircle)
+const (
+
+	SidesTriangle = 3
+	
+	SidesSquare = 4
+	
+	SidesCircle = 0
+
+)
+
+func CalcTriangleSquare(sideLen float64) float64 {
+	// S = (3)**0,5 * a**2 / 4
+	S:= math.Pow(3, 0.5) * math.Pow(sideLen, 2) / 4
+	return S
+}
+
+func CalcSquare_(sideLen float64) float64 {
+	S:= math.Pow(sideLen, 2)
+	return S
+}
+
+func CalcCircleSquare(radius float64) float64 {
+	S:= 2* math.Pi * math.Pow(radius, 2)
+	return S
+}
 
 func CalcSquare(sideLen float64, sidesNum #yourTypeNameHere#) float64 {
+	if sidesNum == 4 {
+		return CalcSquare_(sideLen)
+	} else if sidesNum == 3 {
+		return CalcTriangleSquare(sideLen)
+	} else if sidesNum == 0 {
+		return CalcCircleSquare(sideLen)
+	} else {
+		return 0.0
+	}
+
+
 }
