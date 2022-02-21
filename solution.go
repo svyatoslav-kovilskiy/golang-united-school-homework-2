f7870a1760c8f4d867eeb8d71a64922990987fde
Svyatoslav <svyatoslav_kovylskiy@epam.com>
2022-02-21 16:06:21 +0500

    Added type for second function parametr

--- solution.go
+++ solution.go
@@ -3,6 +3,7 @@ package square
 import (
 	"math"
 )
+
 // Define custom int type to hold sides number and update CalcSquare signature by replacing #yourTypeNameHere#
 
 // Define constants to represent 0, 3 and 4 sides.  Test uses mnemos: SidesTriangle(==3), SidesSquare(==4), SidesCircle(==0)
@@ -11,32 +12,30 @@ import (
 // CalcSquare(10.0, SidesSquare)
 // CalcSquare(10.0, SidesCircle)
 const (
-
 	SidesTriangle = 3
-	
-	SidesSquare = 4
-	
-	SidesCircle = 0
 
+	SidesSquare = 4
+
+	SidesCircle = 0
 )
 
 func CalcTriangleSquare(sideLen float64) float64 {
 	// S = (3)**0,5 * a**2 / 4
-	S:= math.Pow(3, 0.5) * math.Pow(sideLen, 2) / 4
+	S := math.Pow(3, 0.5) * math.Pow(sideLen, 2) / 4
 	return S
 }
 
 func CalcSquare_(sideLen float64) float64 {
-	S:= math.Pow(sideLen, 2)
+	S := math.Pow(sideLen, 2)
 	return S
 }
 
 func CalcCircleSquare(radius float64) float64 {
-	S:= 2* math.Pi * math.Pow(radius, 2)
+	S := 2 * math.Pi * math.Pow(radius, 2)
 	return S
 }
 
-func CalcSquare(sideLen float64, sidesNum #yourTypeNameHere#) float64 {
+func CalcSquare(sideLen float64, sidesNum int) float64 {
 	if sidesNum == 4 {
 		return CalcSquare_(sideLen)
 	} else if sidesNum == 3 {
@@ -46,6 +45,5 @@ func CalcSquare(sideLen float64, sidesNum #yourTypeNameHere#) float64 {
 	} else {
 		return 0.0
 	}
-
 
 }
